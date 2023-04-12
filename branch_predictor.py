ac6429d56813361102f719d34ab82b5f94347195
James Simmons <jsimmons@jmms.me>
2023-04-12 18:55:13 -0400

    Commented out superflous PShare

--- branch_predictor.py
+++ branch_predictor.py
@@ -893,43 +893,43 @@ def run_predictor(predictor: BranchPredictor, filename: str, return_detailed_out
     return num_predictions, num_mispredictions
 
 
-class PShare:
-    def __init__(self, m, n):
-        self.m = m
-        self.n = n
-        self.global_history = 0
-        self.local_history_table = [0] * (2**n)
-        self.prediction_table = [[0] * (2**m) for _ in range(2**n)]
+# class PShare:
+#     def __init__(self, m, n):
+#         self.m = m
+#         self.n = n
+#         self.global_history = 0
+#         self.local_history_table = [0] * (2**n)
+#         self.prediction_table = [[0] * (2**m) for _ in range(2**n)]
 
-    def predict(self, address, outcome):
-        global_history_idx = self.global_history & ((1 << self.n) - 1)
-        local_history_idx = address & ((1 << self.n) - 1)
-        local_history = self.local_history_table[local_history_idx]
+#     def predict(self, address, outcome):
+#         global_history_idx = self.global_history & ((1 << self.n) - 1)
+#         local_history_idx = address & ((1 << self.n) - 1)
+#         local_history = self.local_history_table[local_history_idx]
 
-        prediction_idx = (global_history_idx << self.m) | local_history
-        prediction = self.prediction_table[prediction_idx]
+#         prediction_idx = (global_history_idx << self.m) | local_history
+#         prediction = self.prediction_table[prediction_idx]
 
-        if outcome == "T":
-            prediction = min(prediction + 1, (1 << self.m) - 1)
-        else:
-            prediction = max(prediction - 1, 0)
+#         if outcome == "T":
+#             prediction = min(prediction + 1, (1 << self.m) - 1)
+#         else:
+#             prediction = max(prediction - 1, 0)
 
-        self.prediction_table[prediction_idx] = prediction
+#         self.prediction_table[prediction_idx] = prediction
 
-        self.global_history = ((self.global_history << 1) & ((1 << self.n) - 1)) | (outcome == "T")
-        self.local_history_table[local_history_idx] = ((local_history << 1) & ((1 << self.m) - 1)) | (outcome == "T")
+#         self.global_history = ((self.global_history << 1) & ((1 << self.n) - 1)) | (outcome == "T")
+#         self.local_history_table[local_history_idx] = ((local_history << 1) & ((1 << self.m) - 1)) | (outcome == "T")
 
-        return prediction >= (1 << (self.m - 1))
+#         return prediction >= (1 << (self.m - 1))
     
-    def make_prediction(self, address, branch_is_taken):
-        index = address & ((1 << self.m) - 1)
-        global_history = self.global_history
-        p = self.prediction_table[global_history][index]
-        if branch_is_taken:
-            if p < 2**self.n - 1:
-                self.prediction_table[global_history][index] += 1
-        else:
-            if p > 0:
-                self.prediction_table[global_history][index] -= 1
-        self.global_history = ((self.global_history << 1) + branch_is_taken) & ((1 << self.n) - 1)
-        return p >= 2**(self.n - 1)
+#     def make_prediction(self, address, branch_is_taken):
+#         index = address & ((1 << self.m) - 1)
+#         global_history = self.global_history
+#         p = self.prediction_table[global_history][index]
+#         if branch_is_taken:
+#             if p < 2**self.n - 1:
+#                 self.prediction_table[global_history][index] += 1
+#         else:
+#             if p > 0:
+#                 self.prediction_table[global_history][index] -= 1
+#         self.global_history = ((self.global_history << 1) + branch_is_taken) & ((1 << self.n) - 1)
+#         return p >= 2**(self.n - 1)
