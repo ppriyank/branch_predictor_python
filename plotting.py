e27e5036dfb624634ef615c207dba63eab02e0c9
Priyank Pathak <priyankpathak50@gmail.com>
2023-04-17 15:37:53 -0400

    updates

--- plotting.py
+++ plotting.py
@@ -6,6 +6,7 @@ import matplotlib as mpl
 from matplotlib.lines import Line2D
 import functools
 import matplotlib.patches as patches
+import math
 
 FONTSIZE = 50
 plt.rcParams['xtick.labelsize'] = FONTSIZE
@@ -24,26 +25,30 @@ plotting_size = "Accuracy"
 
 EPSILON = 0.01
 OPACITY = 0.90
-THRESHOLD = 0.02
+# THRESHOLD = 0.02
+THRESHOLD = 0 
+SIZE_THRESHOLD = 0.05
 TRACE_FILES = 'gcc_trace.txt', 'jpeg_trace.txt', 'perl_trace.txt'
 REPETITIONS = 20
-THRESHOLD_TIME = 10
+THRESHOLD_TIME = 30
 IGNORE_ALL = False
 # IGNORE_ALL = True
+# read_default_data = True
+read_default_data = False
 baselines = ["benchmarks5.csv", "benchmarks6.csv", "benchmarks11.csv", "benchmarks13.csv", "benchmarks12.csv"]
 
 Ignored_algorithms = ["GShare_ML-running_mean", "GShare_ML-nearest_pattern2", "GShare_ML-logistic2",
-    "Tournament", "GShare_ML-logistic", "PShare"  ]
+    "Tournament", "GShare_ML-logistic", "PShare", "S_Clustering-skmean" ]
 
 columns=["Tracefile", "Predictor", "Predictor Arguments", "Misprediction Rate", "Accuracy",
 "Precision", "Recall", "F1", "Runtime", "TP", 
 "TN", "FP", "FN", 'Size']
 
 colors = ["blue", "green", "teal", "red", "purple", "violet", "navy", 
-    "cyan", "deeppink", "dodgerblue", "peru", "lime", 
-    "black", "gold", "aqua", "fuchsia", "crimson", "orangered", "yellow", "orange"]
+    "cyan", "crimson", "dodgerblue", "peru", "lime", 
+    "black", "gold", "yellow", "fuchsia", "aqua", "orangered", "deeppink", "orange"]
 
-metrics = ["Accuracy", "Misprediction Rate", 'F1', 'Runtime', "Predictor Arguments"]
+metrics = ["Accuracy", "Misprediction Rate", 'F1', 'Runtime', "Predictor Arguments", 'Size']
 results = pd.read_csv(baselines[0])
 # results = pd.read_csv(baselines[0], header=None, names=columns)
 for file in baselines[1:]:
@@ -102,56 +107,121 @@ def handle_ml(x):
     else:
         return x
 
-
-results = results.apply(handle_running_algorithm, axis=1)
-results = results.apply(handle_ml, axis=1)
-Runtime =  {}     
-Weights = {}
-for trace in TRACE_FILES:
-    if IGNORE_ALL:
-        if len(Runtime) > 1:
-            break 
-    Weights[trace] = {}
-    Runtime[trace] = {}
-    trace_results = results[results["Tracefile"] == trace]
-    algorithms = trace_results.Predictor.unique()
-    for algo in algorithms:
-        if algo in Ignored_algorithms:
-            continue 
+def calc_size(x):
+    if x["Predictor"]  in Ignored_algorithms:
+        x["Size"] = -1
+        return x
+    if x["Predictor"] == "Smith":
+        m = int(x["Predictor Arguments"])
+        x["Size"] = m 
+        return x
+    if x["Predictor"] == "Bimodal":
+        m = int(x["Predictor Arguments"])
+        x["Size"] =  3 * 2**m
+        return x
+    if x["Predictor"] == "Tage":
+        x["Size"] = -1
+        return x
+    if x["Predictor"] == "YehPatt":
+        m,n = [int(e) for e in x["Predictor Arguments"].split(",")]
+        x["Size"] = (n * 2**m) + (2 * 2**n)
+        return x
+    if x["Predictor"] == "GShare":
+        m,n = [int(e) for e in x["Predictor Arguments"].split(",")]
+        x["Size"] = n + (3 * 2**m)
+        return x
+    if x["Predictor"] == "PShare2":
+        m,n = [int(e) for e in x["Predictor Arguments"].split(",")]
+        x["Size"] = n + (((n+1) + 2 * 2**m) * 2**n)
+        return x
+    if x["Predictor"] == "Tournament" or x["Predictor"] == "Tournament2":
+        m,n = [int(e) for e in x["Predictor Arguments"].split(",")]
+        x["Size"] = (n + 3 * 2**m) + (35 + 16 * 2**m) + 2**n
+        return x
+    if x["Predictor"] == "GShare_ML-running_mean2":
+        m,n = [int(e) for e in x["Predictor Arguments"].split(",")]
+        x["Size"] = n + ( (3 + 32)* 2**m)
+        return x
+    if x["Predictor"] == "GShare_ML-nearest_pattern":
+        m,n = [int(e) for e in x["Predictor Arguments"].split(",")]
+        size = n + ( (2**2 + 2**3 + 3) * 2**m)
+        x["Size"] = size
+        return x
+    if x["Predictor"] == "PShare2":
+        m,n = [int(e) for e in x["Predictor Arguments"].split(",")]
+        x["Size"] = n + (((n+1) + 2 * 2**m) * 2**n)
+        return x
+    if x["Predictor"] == "Hybrid":
+        k, g, n, b = [int(e) for e in x["Predictor Arguments"].split(",")]
+        x["Size"] = (3 * 2**b) + (n + (3 * 2**g)) + (2 * 2**k)
+        return x
+    if x["Predictor"] == "Running_logistic" or x["Predictor"] == "Running_Perceptron":
+        m = int(x["Predictor Arguments"])
+        x["Size"] = (1 + 32) * m
+        return x
+    if x["Predictor"] == "GShare_Perceptron":
+        m,n = [int(e) for e in x["Predictor Arguments"].split(",")]
+        x["Size"] = n + ( (2 * n * 32 ) * 2**m)
+        return x
+    
+# GShare Perceptron (m, n) = n + (n * 2**m)
+if read_default_data:
+    results = results.apply(handle_running_algorithm, axis=1)
+    results = results.apply(handle_ml, axis=1)
+    results = results.apply(calc_size, axis=1)
+    FINAL_DF = None
+    Runtime =  {}     
+    Weights = {}
+    for trace in TRACE_FILES:
         if IGNORE_ALL:
-            if len(Runtime[trace]) > 1:
+            if len(Runtime) > 1:
                 break 
-
-        print(f"\n\n {algo} \n\n")
-        Weights[trace][algo] = 1000
-        Vals_Y_to_be_plotted = []  
-        Vals_X_to_be_plotted = []  
-        Runtime[trace][algo] = {}
-        algorithms_trace = trace_results[trace_results.Predictor == algo]
-        if algo != "PShare" and algo != 'S_Clustering-skmean':
-            assert (algorithms_trace.groupby(['Predictor Arguments']).count().FN == REPETITIONS).all()
-        # filtered_df = algorithms_trace[metrics].groupby(['args_string']).mean()
-        filtered_df = algorithms_trace[metrics].groupby(['Predictor Arguments']).agg(custom_average)
-        print(filtered_df)
-        
-        indices = sorted(filtered_df.index, key=functools.cmp_to_key(compare))
-        for args in indices:
-            Weights[trace][algo] = min( Weights[trace][algo], indicator(args))
-            vals = filtered_df[filtered_df.index == args].to_dict(orient='list')
-            curr_y = vals[plotting_y][0]
-            curr_x = vals[plotting_x][0]
-            if curr_x > THRESHOLD_TIME:
+        Weights[trace] = {}
+        Runtime[trace] = {}
+        trace_results = results[results["Tracefile"] == trace]
+        algorithms = trace_results.Predictor.unique()
+        for algo in algorithms:
+            if algo in Ignored_algorithms:
                 continue 
-            if Vals_Y_to_be_plotted != []:
-                closest_y = Vals_Y_to_be_plotted[min(range(len(Vals_Y_to_be_plotted)), key = lambda i: abs(Vals_Y_to_be_plotted[i]-curr_y))]
-                closest_x = Vals_X_to_be_plotted[min(range(len(Vals_X_to_be_plotted)), key = lambda i: abs(Vals_X_to_be_plotted[i]-curr_x))]
-                diff = abs(closest_y - curr_y) + abs(closest_x - curr_x)
-                diff = abs(closest_y - curr_y) 
-                if diff < THRESHOLD:
-                    continue
-            Vals_Y_to_be_plotted.append(curr_y)
-            Vals_X_to_be_plotted.append(curr_x)
-            Runtime[trace][algo][args] = vals
+            if IGNORE_ALL:
+                if len(Runtime[trace]) > 1:
+                    break 
+
+            print(f"\n\n {algo} \n\n")
+            Weights[trace][algo] = 1000
+            Vals_Y_to_be_plotted = []  
+            Vals_X_to_be_plotted = []  
+            Runtime[trace][algo] = {}
+            algorithms_trace = trace_results[trace_results.Predictor == algo]
+            if algo != "PShare" and algo != 'S_Clustering-skmean':
+                assert (algorithms_trace.groupby(['Predictor Arguments']).count().FN == REPETITIONS).all()
+            # filtered_df = algorithms_trace[metrics].groupby(['args_string']).mean()
+            filtered_df = algorithms_trace[metrics].groupby(['Predictor Arguments']).agg(custom_average)
+            filtered_df["Tracefile"] = trace
+            filtered_df["Predictor"] = algo
+            print(filtered_df)
+            if FINAL_DF is None:
+                FINAL_DF = filtered_df
+            else:
+                FINAL_DF = pd.concat([FINAL_DF, filtered_df], ignore_index=True)    
+            indices = sorted(filtered_df.index, key=functools.cmp_to_key(compare))
+            for args in indices:
+                Weights[trace][algo] = min( Weights[trace][algo], indicator(args))
+                vals = filtered_df[filtered_df.index == args].to_dict(orient='list')
+                curr_y = vals[plotting_y][0]
+                curr_x = vals[plotting_x][0]
+                if curr_x > THRESHOLD_TIME:
+                    continue 
+                if Vals_Y_to_be_plotted != []:
+                    closest_y = Vals_Y_to_be_plotted[min(range(len(Vals_Y_to_be_plotted)), key = lambda i: abs(Vals_Y_to_be_plotted[i]-curr_y))]
+                    closest_x = Vals_X_to_be_plotted[min(range(len(Vals_X_to_be_plotted)), key = lambda i: abs(Vals_X_to_be_plotted[i]-curr_x))]
+                    diff = abs(closest_y - curr_y) + abs(closest_x - curr_x)
+                    diff = abs(closest_y - curr_y) 
+                    if diff < THRESHOLD:
+                        continue
+                Vals_Y_to_be_plotted.append(curr_y)
+                Vals_X_to_be_plotted.append(curr_x)
+                Runtime[trace][algo][args] = vals
 
 def plotting1():
     for trace in TRACE_FILES:
@@ -191,7 +261,6 @@ def plotting1():
         plt.savefig(f"{trace}.png")
         plt.clf()  
         
-
 def plotting2(fading=False):
     for trace in TRACE_FILES:
         plt.grid(alpha=0.5)
@@ -233,15 +302,6 @@ def plotting2(fading=False):
         plt.savefig(f"{trace}_F={fading}.png")
         plt.clf()  
         
-
-def line_eq(x_0, y_0, x_1, y_1):
-    if x_0 == x_1:
-        return lambda x: (y_0 + y_1) / 2 + EPSILON
-    else:        
-        m  = (y_1 - y_0) / (x_1 - x_0)
-        b = y_1 - m * x_1
-        return lambda x: m * x + b
-
 def add_arrow(line, position=None, direction='right', size=15, color=None, width=None, headlength=None, headwidth=None):
     if color is None:
         color = line.get_color()
@@ -260,25 +320,6 @@ def add_arrow(line, position=None, direction='right', size=15, color=None, width
     end_x = xdata[0] + 3 * delta_x
     end_y = ydata[0] + 3 * delta_y
 
-    # start_ind = 0
-    # if direction == 'right':
-    #     end_ind = start_ind + 1
-    # else:
-    #     end_ind = start_ind - 1
-    # line.axes.annotate('',
-    #     xytext=(xdata[start_ind], ydata[start_ind]),
-    #     xy=(xdata[end_ind], ydata[end_ind]),
-    #     # arrowprops=dict(arrowstyle="->", color=color, width=width),
-    #     arrowprops=dict(color=color, width=width, headlength=headlength, headwidth=headwidth),
-    #     size=size
-    # )
-    # line.axes.annotate('',
-    #     xytext=(xdata[start_ind], ydata[start_ind]),
-    #     xy=(xdata.mean(), ydata.mean()),
-    #     # arrowprops=dict(arrowstyle="->", color=color, width=width),
-    #     arrowprops=dict(color=color, width=width, headlength=headlength, headwidth=headwidth, alpha=OPACITY),
-    #     size=size
-    # )
     line.axes.annotate('',
         xytext=(start_x, start_y),
         xy=(end_x, end_y),
@@ -287,7 +328,20 @@ def add_arrow(line, position=None, direction='right', size=15, color=None, width
         size=size
     )
 
-def plotting3(fading=False):
+def renaming_name(x):
+    if x == "Tournament2":     
+        return "Tournament"
+    elif x == "PShare2":
+        return "PShare"
+    elif "running_mean2" in x:
+        return x.replace("running_mean2", "running_mean")
+    elif x == "Running_logistic":
+        return "Online Logistic"
+    elif x == "Running_Perceptron":
+        return "Online Perceptron"
+    return x
+    
+def plotting3(draw_arrow=False):
     for trace in TRACE_FILES:
         plt.grid(alpha=0.5)
         # plt.rcParams['font.size'] = 8
@@ -308,18 +362,17 @@ def plotting3(fading=False):
                 area = 3000
                 plt.scatter(x, y, s=area, c=colors[i], alpha=OPACITY, edgecolors='black')    
             if count != 0:
+                algo = renaming_name(algo)
                 labels.append(algo + f' ({max(Y):.2f}) ')
                 h = Line2D([0], [0], marker='o', markersize=np.sqrt(100), color=colors[i], linestyle='None', alpha=OPACITY, markeredgecolor='black')
                 legends.append(h)
-                # plt.plot(X, Y, '-', alpha=OPACITY /2, color=colors[i])
-                for x_0, x_1, y_0, y_1 in zip(X[:-1], X[1:], Y[:-1], Y[1:]): 
-                    # line = line_eq(x_0, y_0, x_1, y_1)
-                    # mid_x = (x_0 + x_1) / 2
-                    # mid_y = (y_0 + y_1) / 2
-                    line = plt.plot([x_0, x_1], [y_0, y_1], '-', alpha=OPACITY /2, color=colors[i])[0]
-                    size = 25
-                    add_arrow(line, color=colors[i], size=900, width=0, headlength=size, headwidth=size)
-
+                if draw_arrow:
+                    for x_0, x_1, y_0, y_1 in zip(X[:-1], X[1:], Y[:-1], Y[1:]): 
+                        line = plt.plot([x_0, x_1], [y_0, y_1], '-', alpha=OPACITY /2, color=colors[i])[0]
+                        size = 25
+                        add_arrow(line, color=colors[i], size=900, width=0, headlength=size, headwidth=size)
+                else:
+                    plt.plot(X, Y, '-', alpha=OPACITY / 2, color=colors[i])
         # plt.legend(legends, labels, loc="lower right", markerscale=2, scatterpoints=0, fontsize=20)
         plt.legend(legends, labels, loc="lower right", markerscale=5, scatterpoints=0, fontsize=FONTSIZE, bbox_to_anchor=(1.44, 0))
         
@@ -328,14 +381,99 @@ def plotting3(fading=False):
         plt.ylabel(f"{plotting_y} Scores")
         plt.subplots_adjust(right=0.7, left=0.05, top=0.95, bottom=0.09)
         # plt.tight_layout()
-        plt.savefig(f"{trace}_F={fading}.png")
+        plt.savefig(f"{trace}.png")
         plt.clf()  
         if IGNORE_ALL:
             break 
 
+def plotting4():
+    Ratios = {}
+    for trace in TRACE_FILES:
+        plt.grid(alpha=0.5)
+        legends = []
+        labels = []
+        Ratios[trace] = {}
+        max_ratio = -1
+        max_ratio_algo = -1
+        for i,algo in enumerate(Runtime[trace].keys()):
+            count = 0 
+            Ratios[trace][algo] = {}
+            for args in Runtime[trace][algo].keys():
+                count += 1
+                y = Runtime[trace][algo][args][plotting_y][0]
+                x = Runtime[trace][algo][args][plotting_x][0]
+                ratio = y / x
+                Ratios[trace][algo][args] = ratio 
+                if ratio > max_ratio:
+                    max_ratio_algo = (algo, args)
+        print(max_ratio_algo)
+    return Ratios
+
+def plotting5(FINAL_DF, y_axis_label="acc"):
+    plt.figure(figsize=(35, 30))
+    for trace in TRACE_FILES:
+        plt.grid(alpha=0.5)
+        legends = []
+        labels = []
+        trace_results = FINAL_DF[FINAL_DF["Tracefile"] == trace]
+        algorithms = trace_results.Predictor.unique()
+        for i,algo in enumerate(algorithms):
+            algorithms_trace = trace_results[trace_results.Predictor == algo]
+            X = algorithms_trace.Size
+            Y = algorithms_trace[plotting_y]
+            Z = algorithms_trace[plotting_y] / algorithms_trace.Runtime
+            X_clean = []
+            Y_clean = []
+            Z_clean = []
+            for x,y,a,b in zip(X[:-1], X[1:], Y[:-1], Z[:-1]):
+                if y- x < SIZE_THRESHOLD:
+                    continue
+                else:
+                    X_clean.append(x)
+                    Y_clean.append(a)
+                    Z_clean.append(b)
+            
+            # plt.plot(X, Y, '-o', alpha=OPACITY / 2, color=colors[i])
+            algo = renaming_name(algo)
+            if y_axis_label == "acc":
+                plt.plot(X_clean, Y_clean, 'o--', color=colors[i], label=algo, markersize=20, linewidth =5, markeredgecolor='black')
+            else:
+                plt.plot(X_clean, Z_clean, 'o--', color=colors[i], label=algo, markersize=20, linewidth =5, markeredgecolor='black')
+
+        plt.legend()
+        # plt.legend(legends, labels, loc="lower right", markerscale=5, scatterpoints=0, fontsize=FONTSIZE, bbox_to_anchor=(1.44, 0))
+        
+        plt.title("BenchMarking: " + r"$\bf{" + str(trace.replace("_", "\_")) + "}$" + f", Thres. skip {SIZE_THRESHOLD}, t <{THRESHOLD_TIME}s")
+        plt.xlabel("Log2 (Size)")
+        if y_axis_label == "acc":
+            plt.ylabel(f"{plotting_y} Scores")
+        else:
+            plt.ylabel("Accuracy / Time Ratio")
+        plt.tight_layout()
+        # plt.subplots_adjust(right=0.7, left=0.05, top=0.95, bottom=0.09)
+        plt.savefig(f"SIZE_{trace}.png")
+        plt.clf()  
+        
+
+
+
 # plotting2(fading=False)
 # plotting2(fading=True)
-plotting3(fading=False)
+# plotting3(draw_arrow=False)
 
 # conda activate bert
 # python plotting.py
+
+# ratios = plotting4()
+# 
+if read_default_data:
+    FINAL_DF.to_csv("size.csv", index=False)
+
+
+FINAL_DF = pd.read_csv("size.csv")
+FINAL_DF = FINAL_DF.sort_values(by=['Size'])
+FINAL_DF.Size = np.log(FINAL_DF.Size) / np.log(2)
+FINAL_DF = FINAL_DF[FINAL_DF.Size < THRESHOLD_TIME ]
+
+plotting5(FINAL_DF, y_axis_label="ratio")
+# plotting5(FINAL_DF)
