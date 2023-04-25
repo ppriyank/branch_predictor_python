f25f4dcc0d1bd9b02ca7fdd72a6c9bd8ca1ffa9d
Priyank Pathak <priyankpathak50@gmail.com>
2023-04-24 23:39:52 -0400

    backup

--- to_be_submitted/run.sh
+++ to_be_submitted/run.sh
@@ -7,7 +7,7 @@ sim gshare 8 6 traces/gcc_trace.txt
 
 sim hybrid 5 4 4 4 traces/gcc_trace.txt
 
-
+python smith.py 2 traces/gcc_trace.txt
 sim smith 2 traces/gcc_trace.txt > chikka.txt
 sim bimodal 5 traces/gcc_trace.txt > chikka.txt
 sim gshare 8 6 traces/gcc_trace.txt > chikka.txt
